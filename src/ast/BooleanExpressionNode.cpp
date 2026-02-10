550b66cd6af11ba119e25f47ef7d06813a8111b8
writemorecode <gustav.ae.karlsson@gmail.com>
2026-02-10 15:56:37 +0100

    Fix: short-circuit IR for && and ||
    Bug: AndNode/OrNode eagerly evaluated both operands and emitted boolean TAC, violating short-circuit semantics. This could execute RHS effects or faults even when LHS decides the result.\n\nFix: generate explicit control-flow blocks for RHS evaluation and true/false assignment, then join with the computed temporary boolean.

--- src/ast/BooleanExpressionNode.cpp
+++ src/ast/BooleanExpressionNode.cpp
@@ -1,20 +1,76 @@
 #include "ast/BooleanExpressionNode.hpp"
-#include "ir/BooleanTac.hpp"
+#include "ir/Tac.hpp"
 
 Operand AndNode::generateIR(CFG &graph, SymbolTable &st) {
-    auto lhs_name = left->generateIR(graph, st);
-    auto rhs_name = right->generateIR(graph, st);
+    auto lhsName = left->generateIR(graph, st);
     auto name = graph.getTemporaryName();
     st.addBooleanVariable(name);
-    graph.addInstruction(new AndTac(name, lhs_name, rhs_name));
+
+    auto *rhsEvalBlock = graph.newBlock();
+    auto *trueBlock = graph.newBlock();
+    auto *falseBlock = graph.newBlock();
+    auto *joinBlock = graph.newBlock();
+
+    auto *entryBlock = graph.getCurrentBlock();
+    graph.addInstruction(new CondJumpTac(falseBlock->getName(), lhsName));
+    graph.addInstruction(new JumpTac(rhsEvalBlock->getName()));
+    entryBlock->setTrueBlock(rhsEvalBlock);
+    entryBlock->setFalseBlock(falseBlock);
+
+    graph.setCurrentBlock(rhsEvalBlock);
+    auto rhsName = right->generateIR(graph, st);
+    graph.addInstruction(new CondJumpTac(falseBlock->getName(), rhsName));
+    graph.addInstruction(new JumpTac(trueBlock->getName()));
+    rhsEvalBlock->setTrueBlock(trueBlock);
+    rhsEvalBlock->setFalseBlock(falseBlock);
+
+    graph.setCurrentBlock(trueBlock);
+    graph.addInstruction(new CopyTac(1, name));
+    graph.addInstruction(new JumpTac(joinBlock->getName()));
+    trueBlock->setTrueBlock(joinBlock);
+
+    graph.setCurrentBlock(falseBlock);
+    graph.addInstruction(new CopyTac(0, name));
+    graph.addInstruction(new JumpTac(joinBlock->getName()));
+    falseBlock->setTrueBlock(joinBlock);
+
+    graph.setCurrentBlock(joinBlock);
     return name;
 }
 
 Operand OrNode::generateIR(CFG &graph, SymbolTable &st) {
-    auto lhs_name = left->generateIR(graph, st);
-    auto rhs_name = right->generateIR(graph, st);
+    auto lhsName = left->generateIR(graph, st);
     auto name = graph.getTemporaryName();
     st.addBooleanVariable(name);
-    graph.addInstruction(new OrTac(name, lhs_name, rhs_name));
+
+    auto *rhsEvalBlock = graph.newBlock();
+    auto *trueBlock = graph.newBlock();
+    auto *falseBlock = graph.newBlock();
+    auto *joinBlock = graph.newBlock();
+
+    auto *entryBlock = graph.getCurrentBlock();
+    graph.addInstruction(new CondJumpTac(rhsEvalBlock->getName(), lhsName));
+    graph.addInstruction(new JumpTac(trueBlock->getName()));
+    entryBlock->setTrueBlock(trueBlock);
+    entryBlock->setFalseBlock(rhsEvalBlock);
+
+    graph.setCurrentBlock(rhsEvalBlock);
+    auto rhsName = right->generateIR(graph, st);
+    graph.addInstruction(new CondJumpTac(falseBlock->getName(), rhsName));
+    graph.addInstruction(new JumpTac(trueBlock->getName()));
+    rhsEvalBlock->setTrueBlock(trueBlock);
+    rhsEvalBlock->setFalseBlock(falseBlock);
+
+    graph.setCurrentBlock(trueBlock);
+    graph.addInstruction(new CopyTac(1, name));
+    graph.addInstruction(new JumpTac(joinBlock->getName()));
+    trueBlock->setTrueBlock(joinBlock);
+
+    graph.setCurrentBlock(falseBlock);
+    graph.addInstruction(new CopyTac(0, name));
+    graph.addInstruction(new JumpTac(joinBlock->getName()));
+    falseBlock->setTrueBlock(joinBlock);
+
+    graph.setCurrentBlock(joinBlock);
     return name;
 }
